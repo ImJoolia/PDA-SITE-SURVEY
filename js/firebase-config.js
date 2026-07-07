f25d110adcdf2ff79f3eb384470ec656db155290
Julia <100211456+ImJoolia@users.noreply.github.com>
2026-07-07 15:29:40 -0600

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -16,10 +16,10 @@
 // ============================================================
 
 export const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_PROJECT.appspot.com",
-  messagingSenderId: "PASTE_YOUR_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID",
+  apiKey: "AIzaSyAvvLQG-Zn38NcCuaqXLRcEPeOtgz_ON48",
+  authDomain: "pda-site-survey.firebaseapp.com",
+  projectId: "pda-site-survey",
+  storageBucket: "pda-site-survey.firebasestorage.app",
+  messagingSenderId: "622639170994",
+  appId: "1:622639170994:web:63ed71fb56ab77b99c50ab",
 };
